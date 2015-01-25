2feb4f92d10c5b3f70e4aba0ff3493c851b76c2b
Florian Eiden <florian.eiden@fleid.fr>
2015-01-25 13:29:16 +0100

    Fuzzy Lookup?

--- RR_Peer2.R
+++ RR_Peer2.R
@@ -5,7 +5,8 @@ setwd("~/Documents/GitHub/RepData_PeerAssessment2")
 Sys.setlocale("LC_TIME", "en_US")
 
 library(ggplot2)
-library(R.utils)
+library(grid)
+library(R.utils) #bunzip2
 
 sessionInfo()
 
@@ -19,8 +20,41 @@ bunzip2("StormData.csv.bz2", overwrite = FALSE, remove = FALSE)
 
 SD_Raw <- read.csv("StormData.csv", header = TRUE, sep = ",")
 
+EVTYPE_Ref <- read.csv("EVTYPE_Ref.txt",header = FALSE, sep =",")
+
 # Question 1
 # Across the United States, which types of events (as indicated in the EVTYPE variable) are most harmful with respect to population health?
+summary(SD_Raw)
+
+h1 <- aggregate(
+     cbind(SD_Raw$FATALITIES,SD_Raw$INJURIES)
+    ,by=list(
+            format(as.Date(SD_Raw$BGN_DATE,"%m/%d/%Y"),"%Y")
+            ,as.character(SD_Raw$EVTYPE)
+      )
+    ,FUN=sum
+  )
+
+names(h1) <- c("YEAR","EVTYPE","FATALITIES","INJURIES") 
+
+h1_modern <- subset(h1,h1$YEAR >= 1990)
+
+q1 <- ggplot(h1_modern,aes(x=YEAR, y=INJURIES, colour=YEAR))
+q1 <- q1 + geom_bar(stat="identity") +ylim(0,10000)
+
+q2 <- ggplot(h1_modern,aes(x=YEAR, y=FATALITIES))
+q2 <- q2 + geom_bar(stat="identity") +ylim(0,10000)
+
+pushViewport(viewport(layout = grid.layout(1, 2)))
+print(q1, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+print(q2, vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
+
+
+
+EVLIST <- unique(h1_modern$EVTYPE[h1_modern$FATALITIES + h1_modern$INJURIES>0])
+
+
+
 
 
 # Question 2
